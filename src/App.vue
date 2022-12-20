5f6e2905f6c001c2f66dbfa102587accf7043f69
SergeyKudinov <serezha.kudinov.1995@mail.ru>
2022-12-21 04:09:52 +0800

    add sort component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="app">
         <h1>Страница с постами</h1>
-        <div>
-            
+        <div class="app_btns">
+            <my-button
+                @click="showDialog"
+            >
+                Создать пост
+            </my-button>
+            <MySelect
+                v-model="selectedSort"
+                :options="sortOptions"
+            >
+            </MySelect>
         </div>
-        <my-button
-            style="margin: 15px 0"
-            @click="showDialog"
-        >
-            Создать пост
-        </my-button>
         <my-dialog v-model:show="dialogVisible">
             <PostForm
                 @create="createPost"
@@ -28,17 +31,24 @@
     import PostList from './components/PostList.vue';
     import PostForm from './components/PostForm.vue';
     import axios from 'axios';
+    import MySelect from './components/UI/MySelect.vue';
 
     export default {
         components: {
     PostList,
     PostForm,
+    MySelect
 },
         data() {
             return{
                 posts: [],
                 dialogVisible: false,
                 isPostsLoading: false,
+                selectedSort: '',
+                sortOptions: [
+                    {value: 'title', name: 'По названию'},
+                    {value: 'body', name: 'По описанию'},
+                ]
             }
         },
         methods: {
@@ -79,5 +89,9 @@
     .app {
         padding: 20px;
     }
-    
+    .app_btns {
+        margin: 15px 0;
+        display: flex;
+        justify-content: space-between;
+    }
 </style>
